eb07b8017b618832d9519273b3921cd5da14d2ee
Food4Less <Guccirich2@gmail.com>
2022-12-06 11:45:05 -0800

    Continued my Rubys Adventure project
    Added more tiles and completed some tutorial

--- RubysAdventureNichols/Assets/Scripts/RubyController.cs
+++ RubysAdventureNichols/Assets/Scripts/RubyController.cs
@@ -5,26 +5,36 @@ using UnityEngine.UIElements;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    void Update() 
+    void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+
         //create 2 variables to use the unity built in axes
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         //Create my movement vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //Create the movement of my character
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         //Set the new position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
